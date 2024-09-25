63e30ab386031290a0388a0c038e6830b2cc0a05
Wilfredo Porta <wporta@gmail.com>
2024-09-24 23:12:32 -0600

    Initial commit. Spring Boot project creation.

---
+++ src/main/java/org/wporta/conmed_api/ConmedApiApplication.java
@@ -0,0 +1,13 @@
+package org.wporta.conmed_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConmedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConmedApiApplication.class, args);
+	}
+
+}
